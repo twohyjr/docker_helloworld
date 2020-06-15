4282fa982ac2403e86f16d51ad00655ca3f771f5
Rick Twohy <twohyjr@gmail.com>
2020-06-15 19:23:21 -0400

    docker login 3

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ aws configure set default.region ${EB_REGION}
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure set default.region ${EB_REGION}
-aws ecr get-login-password | docker login --username AWS --password-stdin ${REGISTRY_URL}
+aws ecr get-login-password | docker login --username AWS --password-stdin 926110576269.dkr.ecr.us-east-1.amazonaws.com
 
 # update latest version
 docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
