cfd6da2cb52e4035f6ccd65e71c2d32d9dc72eeb
Rick Twohy <twohyjr@gmail.com>
2020-06-15 23:54:15 -0400

    updated test

--- test.sh
+++ test.sh
@@ -5,8 +5,6 @@ HTTP_BODY=$(curl http://a8305d7693ea04868ba1d66ee1441a0d-906805300.us-east-1.elb
 str1="Hello, World!"
 str2=$HTTP_BODY
 
-echo $HTTP_BODY
-
 if [ "$str1" != "$str2" ]; then
     echo "Strings are not equal"
     exit 1
