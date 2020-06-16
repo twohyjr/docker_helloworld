0c34c91f0ef5f1e27ef3b39d9957f7cf89e6f2be
Rick Twohy <twohyjr@gmail.com>
2020-06-15 22:36:30 -0400

    prob wont work

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ aws ecr get-login-password | docker login --username AWS --password-stdin 926110
 docker build -t 926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest .
 docker push 926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest
 
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl set image deployment/helloworld-deployment helloworld=926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest
 
 
