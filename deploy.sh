a9d3e9d5b1c62712b8fc4be9f7e18c3c7833768c
Rick Twohy <twohyjr@gmail.com>
2020-06-15 21:20:56 -0400

    updated deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,31 @@
-#!/bin/bash -e
-
-# this is most likely namespaced repo name like myorg/veryimportantimage
-SOURCE_IMAGE=926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld
-# using it as there will be 2 versions published
-TARGET_IMAGE=926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld
-
-# lets make sure we always have access to latest image
-TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
-
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-aws configure set default.region ${EB_REGION}
-aws ecr get-login-password | docker login --username AWS --password-stdin 926110576269.dkr.ecr.us-east-1.amazonaws.com
+aws configure set default.region us-east-1
 
-# Push image to ECR
-###################
-docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
-docker push ${TARGET_IMAGE_LATEST}
+aws ecr get-login-password | docker login --username AWS --password-stdin 926110576269.dkr.ecr.us-east-1.amazonaws.com
+docker build -t 926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest .
+docker push 926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest
+
+
+
+
+
+# #!/bin/bash -e
+
+# # this is most likely namespaced repo name like myorg/veryimportantimage
+# SOURCE_IMAGE=926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld
+# # using it as there will be 2 versions published
+# TARGET_IMAGE=926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld
+
+# # lets make sure we always have access to latest image
+# TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
+
+# aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+# aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+# aws configure set default.region ${EB_REGION}
+# aws ecr get-login-password | docker login --username AWS --password-stdin 926110576269.dkr.ecr.us-east-1.amazonaws.com
+
+# # Push image to ECR
+# ###################
+# docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_LATEST}
+# docker push ${TARGET_IMAGE_LATEST}
