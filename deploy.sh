2df67e2c40addf2c95f6898cb575c17bed8b10f9
Rick Twohy <twohyjr@gmail.com>
2020-06-15 23:31:50 -0400

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
+# setup aws configuration
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 aws configure set default.region us-east-1
 
+# build/push image to aws repo
 aws ecr get-login-password | docker login --username AWS --password-stdin 926110576269.dkr.ecr.us-east-1.amazonaws.com
 docker build -t 926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest .
 docker push 926110576269.dkr.ecr.us-east-1.amazonaws.com/helloworld:latest
-aws eks update-kubeconfig --name helloworld-cluster
 
+# restart the deployment object
+aws eks update-kubeconfig --name helloworld-cluster
 kubectl rollout restart deployment/helloworld-deployment
